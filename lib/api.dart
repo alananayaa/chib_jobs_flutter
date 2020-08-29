620211b278a5299d8133b4bf2eb13f59b445ee11
Alan Anaya Araujo <anaya.araujo@icloud.com>
2020-08-28 21:09:50 -0500

    missing files

--- lib/api.dart
+++ lib/api.dart
@@ -1,11 +1,7 @@
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
-<<<<<<< Updated upstream
-const baseUrl = 'https://wise-happy-ferryboat.glitch.me';
-=======
 const baseUrl = 'https://wise-happy-ferryboat.glitch.me/';
->>>>>>> Stashed changes
 
 class API {
   static Future getUsers() {
